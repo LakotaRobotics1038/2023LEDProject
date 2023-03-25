9a88cf08eddb9b37beb680fba68ad6c7f337f45c
Ayden Longbottom <ayden.allin3d@gmail.com>
2023-03-25 10:50:54 -0400

    Daves new code

--- code.py
+++ code.py
@@ -1,144 +1,154 @@
 import time
 import board
-import neopixel
-import math
+import digitalio
 import busio
+import usb_cdc
+import neopixel
+from rainbowio import colorwheel
+from digitalio import DigitalInOut, Direction, Pull
 
-from adafruit_led_animation.animation.rainbow import Rainbow
+ORDER = neopixel.GRB
+Alliance = "" # "R"ed or "B"lue or "" Unknown 
+AnimationFrame = 0
 
-# Define the number of pixels in the LED strips.
-NUM_PIXELS_1 = 86
-NUM_PIXELS_2 = 15
-NUM_PIXELS_3 = 46
-NUM_PIXELS_4 = 15
+ColorMode_1038 = 0
+ColorMode_Cone = 1
+ColorMode_Cube = 2
+ColorMode_Confirmed = 3
+ColorMode_Ukraine = 4
+ColorMode_Rainbow = 5
 
-# Define the colors to be used in the wave effect.
-COLOR1 = (200, 0, 200)
-COLOR2 = (0, 0, 200)
+ColorMode = ColorMode_1038 # Initialize
+BluePurpleLength = 8
 
-# Define the speed of the wave effect.
-SPEED = 1
-
-# Define the length of each color segment.
-BLUE_SEG_LEN = 8
-PURPLE_SEG_LEN = 8
-
-# Initialize the NeoPixel strips.
-pixels1 = neopixel.NeoPixel(board.GP2, NUM_PIXELS_1, brightness=0.5, auto_write=False)
-pixels2 = neopixel.NeoPixel(board.GP3, NUM_PIXELS_2, brightness=0.5, auto_write=False)
-pixels3 = neopixel.NeoPixel(board.GP4, NUM_PIXELS_3, brightness=0.5, auto_write=False)
-pixels4 = neopixel.NeoPixel(board.GP5, NUM_PIXELS_4, brightness=0.5, auto_write=False)
-
-
-# Initialize the serial communication.
+serial = usb_cdc.data
 uart = busio.UART(board.GP0, board.GP1, baudrate=9600)
 
-# Initialize the rainbow animation.
-rainbow = Rainbow(pixels1, speed=0.1, period=5)
-rainbow = Rainbow(pixels2, speed=0.1, period=5)
-rainbow = Rainbow(pixels3, speed=0.1, period=5)
-rainbow = Rainbow(pixels4, speed=0.1, period=5)
+switch = DigitalInOut(board.GP11)
+switch.direction = Direction.INPUT
+switch.pull = Pull.UP
 
-# Initialize the mode variable.
-mode = "wave"
+# Define the number of pixels in the LED strips.
+RIGHT_SIDE = 151
+NICH_KNACK = 15
+LEFT_SIDE = 151
+LEFT_SIDE_FIX = 100
+X_FRAME = 69
 
-# Define the wave effect function.
-def wave(color1, color2, speed, blue_len, purple_len):
-    # Calculate the current time.
-    t = time.monotonic() * speed
+pixels1 = neopixel.NeoPixel(board.GP2, RIGHT_SIDE, brightness=0.8, auto_write=False)
+pixels2 = neopixel.NeoPixel(board.GP3, NICH_KNACK, brightness=0.8, auto_write=False)
+pixels3 = neopixel.NeoPixel(board.GP4, LEFT_SIDE, brightness=0.8, auto_write=False)
+pixels4 = neopixel.NeoPixel(board.GP5, LEFT_SIDE_FIX, brightness=0.8, auto_write=False)
+pixels5 = neopixel.NeoPixel(board.GP6, X_FRAME, brightness=0.8, auto_write=False)
 
-    # Loop through each pixel in the first strip.
-    for i in range(NUM_PIXELS_1):
-        # Calculate the sine value for the current pixel.
-        sine_value = math.sin(i * 0.5 + t)
+def GetChar():                   # Get any color change requests from the USB or RoboRio serial port
+   Result = ""
+   if serial and serial.in_waiting > 0:
+      byte = serial.read(1)
+      print("USB: ", byte)
+      Result = byte
+   if uart.in_waiting > 0:        
+      byte = uart.read(1)
+      print("RoboRio: ", byte)
+      Result = byte
+   return Result if Result else ""
 
-        # Set the pixel color based on the sine value and segment length.
-        if i % (blue_len + purple_len) < blue_len:
-            if sine_value > 0:
-                pixels1[i] = tuple(int(c * sine_value) for c in color1)
-            else:
-                pixels1[i] = tuple(int(c * abs(sine_value)) for c in color2)
-        else:
-            if sine_value > 0:
-                pixels1[i] = tuple(int(c * sine_value) for c in color2)
-            else:
-                pixels1[i] = tuple(int(c * abs(sine_value)) for c in color1)
+def FillAllianceColor():
+   if (Alliance == "R" ):
+      for i in range(35, 48):         
+         pixels1[i] = (255, 0, 0)
+         pixels4[i] = (255, 0, 0)
+   if (Alliance == "B" ):
+      for i in range(35, 48):
+         pixels1[i] = (0, 0, 255)
+         pixels4[i] = (0, 0, 255)   
 
-    # Loop through each pixel in the second strip.
-    for i in range(NUM_PIXELS_2):
-        # Calculate the sine value for the current pixel.
-        sine_value = math.sin(i * 0.5 + t)
+while True: 
+   Incoming = GetChar()
+   if Incoming != "":
+      if (Incoming == b'R'):
+         print("Red")
+         Alliance = "R"
+      if (Incoming == b'B'):
+         print("Blue")
+         Alliance = "B"
+      if (Incoming == b'G'):
+         print("Green")
+         ColorMode = ColorMode_Confirmed
+         AnimationFrame = 0
+      if (Incoming == b'Y'):
+         print("Cone")
+         ColorMode = ColorMode_Cone
+         AnimationFrame = 0
+      if (Incoming == b'P'):
+         print("Cube")
+         ColorMode = ColorMode_Cube
+         AnimationFrame = 0
+      if (Incoming == b'U'):
+         print("Ukraine")
+         ColorMode = ColorMode_Ukraine
+      if (Incoming == b'#'):
+         print("1038")
+         ColorMode = ColorMode_1038
+      if (Incoming == b'!'):
+         print("Rainbow")
+         ColorMode = ColorMode_Rainbow
 
-        # Set the pixel color based on the sine value and segment length.
-        if i % (blue_len + purple_len) < blue_len:
-            if sine_value > 0:
-                pixels2[i] = tuple(int(c * sine_value) for c in color1)
-            else:
-                pixels2[i] = tuple(int(c * abs(sine_value)) for c in color2)
-        else:
-            if sine_value > 0:
-                pixels2[i] = tuple(int(c * sine_value) for c in color2)
-            else:
-                pixels2[i] = tuple(int(c * abs(sine_value)) for c in color1)
+   if (ColorMode == ColorMode_Cube):
+      if (switch.value):
+         for i in range(0, 35):
+            pixels1[i] = (152, 51, 250)
+            pixels4[i] = (152, 51, 250)
+      else:
+         for i in range(0, 35):
+            pixels1[i] = (0, 0, 0)
+            pixels4[i] = (0, 0, 0)
 
-                    # Loop through each pixel in the second strip.
-    for i in range(NUM_PIXELS_3):
-        # Calculate the sine value for the current pixel.
-        sine_value = math.sin(i * 0.5 + t)
+   if (ColorMode == ColorMode_Cone): 
+      if (switch.value):
+         for i in range(0, 35):
+            pixels1[i] = (254, 214, 0)
+            pixels4[i] = (254, 214, 0)
+      else:
+         for i in range(0, 35):
+            pixels1[i] = (0, 0, 0)
+            pixels4[i] = (0, 0, 0)
 
-        # Set the pixel color based on the sine value and segment length.
-        if i % (blue_len + purple_len) < blue_len:
-            if sine_value > 0:
-                pixels3[i] = tuple(int(c * sine_value) for c in color1)
-            else:
-                pixels3[i] = tuple(int(c * abs(sine_value)) for c in color2)
-        else:
-            if sine_value > 0:
-                pixels3[i] = tuple(int(c * sine_value) for c in color2)
-            else:
-                pixels3[i] = tuple(int(c * abs(sine_value)) for c in color1)
+   if (ColorMode == ColorMode_Ukraine):
+      for i in range(0, 17):         
+         pixels1[i] = (254,214,0)
+         pixels1[i+18] = (0,197,187) 
+         pixels4[i] = (254,214,0)
+         pixels4[i+18] = (0,197,187) 
 
-                    # Loop through each pixel in the second strip.
-    for i in range(NUM_PIXELS_4):
-        # Calculate the sine value for the current pixel.
-        sine_value = math.sin(i * 0.5 + t)
+   if (ColorMode == ColorMode_1038):
+      for i in range(0, RIGHT_SIDE):         
+         if (((i + AnimationFrame) % (BluePurpleLength * 2)) >= BluePurpleLength):
+            pixels1[i] = (128,0,128)
+         else:
+            pixels1[i] = (0,0,128)
 
-        # Set the pixel color based on the sine value and segment length.
-        if i % (blue_len + purple_len) < blue_len:
-            if sine_value > 0:
-                pixels4[i] = tuple(int(c * sine_value) for c in color1)
-            else:
-                pixels4[i] = tuple(int(c * abs(sine_value)) for c in color2)
-        else:
-            if sine_value > 0:
-                pixels4[i] = tuple(int(c * sine_value) for c in color2)
-            else:
-                pixels4[i] = tuple(int(c * abs(sine_value)) for c in color1)
+   if (ColorMode == ColorMode_Confirmed):
+      for i in range(0, RIGHT_SIDE):         
+         pixels1[i] = (0, 214, 0)
+      for i in range(0, LEFT_SIDE_FIX):                           
+         pixels4[i] = (0, 214, 0)      
 
-    # Update the LED strips.
-    pixels1.show()
-    pixels2.show()
-    pixels3.show()
-    pixels4.show()
+   if (ColorMode == ColorMode_Rainbow):
+      for i in range(0, RIGHT_SIDE):                  
+         rc_index = (i * 256 // RIGHT_SIDE) + AnimationFrame * 16
+         pixels1[i] = colorwheel(rc_index & 255)
 
-# Loop forever and run the wave or rainbow animation.
-while True:
-    # Check if there is any data received over serial communication.
-    if uart.in_waiting > 0:
-        # Read the data from the serial communication.
-        data = uart.read(1)
-        print(data)
+   FillAllianceColor()
+   
+   pixels1.show()
+   pixels2.show()
+   pixels3.show()
+   pixels4.show()
+   pixels5.show()
 
-        # Check if the data is "E".
-        if data == b'E':
-            # Switch to the rainbow animation mode.
-            mode = "rainbow"
-
-    # Check the current animation mode.
-    if mode == "wave":
-        # Run the wave animation.
-        wave(COLOR1, COLOR2, SPEED, BLUE_SEG_LEN, PURPLE_SEG_LEN)
-
-    elif mode == "rainbow":
-        # Run the rainbow animation.
-        rainbow.animate()
+   AnimationFrame = AnimationFrame + 1
+   if AnimationFrame > 15:
+      AnimationFrame = 0
+      if (ColorMode == ColorMode_Confirmed):  # After a little while of "Green", go back to 1038 animation
+         ColorMode = ColorMode_1038
